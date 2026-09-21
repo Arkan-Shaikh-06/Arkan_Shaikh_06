93f83432db412d000b254b989eaf71355ee7e2cb
Arkan Kamarali Shaikh <arkanshaikh531@gmail.com>
2026-09-21 10:37:03 +0530

    Add files via upload

---
+++ experiment05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+//This is a single-line comment
+int age = 18; //Delcaring and initializing age
+/*
+This is a multi-line comment
+It can contain multiple lines.
+*/
+printf("My age is: %d\n", age);
+return 0;
+}
